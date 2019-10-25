8705f812c2b39d8d404f26f5bd36c99e8bf59bed
Stacey Koumaras <stacey_k@tpg.com.au>
2019-10-26 00:59:19 +1100

    homework

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 var length = prompt("Choose a length between 8 and 128 for your password");
 
-alert("The length you have chosen is:" + length);
+alert("The length you have chosen is: " + length);
 
 var specialCharacters = confirm("Would you like to use special characters in your password");
 
 var numCharacters = confirm("Would you like to use numerical characters in your password?");
 
-var lowercase = confirm("Would you like to use lowercase charcaters in your password?");
+var lowercase = confirm("Would you like to use lowercase characters in your password?");
 
 var uppercase = confirm("Would you like to use uppercase characters in your password?");
 
-//function CopyToClipboard(containerid) {
-    //if (document.selection) {
-      //var range = document.body.createTextRange();
-      //range.moveToElementText(document.getElementById(containerid));
-      //range.select().createTextRange();
-      //document.execCommand("copy");
-  
-    //} else if (window.getSelection) {
-      //var range = document.createRange();
-      //range.selectNode(document.getElementById(containerid));
-      //window.getSelection().addRange(range);
-      //document.execCommand("copy");
-      //alert("text copied, copy in the text-area")
-    //}
-  //}
 
-//var lowercaseInput = document.getElementById("lowercase");
+            
+function randStr(len) {
+    let s = '';
+    while (s.length < len) s += Math.random().toString(36).substr(2, len - s.length);
+    return s;
+    }
 
+    // usage
+        console.log(randStr(50));
 
+        alert("Your password is " +  (randStr(50)));
+
+            
+    
+
+//Generate the password with all the info
 //function generate (){
 
     //let values= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
@@ -43,23 +41,5 @@ var uppercase = confirm("Would you like to use uppercase characters in your pass
 //}
 
 
-
-//var lowercase = "abcdefghijklmnopqrstuvwxyz";
-
-//var userPassword;
-//var passwordCharSet;
-  
-//function generate() {
- //  userPassword = "";
- //  passwordCharSet = "";
- //  if (lowercaseInput.checked) {
-  //    passwordCharSet += lowercase;
-  //  }
-    
-//    plength = Number(lengthInput.value);
-   
- //   for (let i = 0; i < plength; i++) {
- //     userPassword += passwordCharSet.charAt(
- //       Math.floor(Math.random() * passwordCharSet.length)
-  //    );
-  //  }
+//Display the password in an alert or on the page
+alert("Your password is" + password)
